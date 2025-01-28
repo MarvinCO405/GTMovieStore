52390024995baf1c5d4b7aae1cbd85e1ece7eea2
Jake904 <jabujawdeh10@gmail.com>
2025-01-27 19:40:28 -0500

    Created a simple shell for an about page

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('', views.index, name='home.python manage.py runserverindex'),
+    path('about',views.about, name = 'home.about'),
 ]
