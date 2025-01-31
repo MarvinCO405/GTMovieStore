e1d6416fb23a54e99caa909ce4d6c9223f7f64f0
Jake904 <jabujawdeh10@gmail.com>
2025-01-31 07:07:42 -0500

    SqLLite Database Set-Up and Sign up, Log in, and Log out Features Implemented

--- GTMovieStore/settings.py
+++ GTMovieStore/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'movies',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'GTMovieStore/static/',
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
