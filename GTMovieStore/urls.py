6479a5c009c34bc1b244031b9e6566f746fafe26
MarvinCO405 <129649132+MarvinCO405@users.noreply.github.com>
2025-01-30 23:35:45 -0500

    Chapter 5 Work

--- GTMovieStore/urls.py
+++ GTMovieStore/urls.py
@@ -16,6 +16,8 @@ Including another URLconf.
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     #test
@@ -24,3 +26,5 @@ urlpatterns = [
     # path('accounts/', include('accounts.urls')),
     path('movies/', include('movies.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
